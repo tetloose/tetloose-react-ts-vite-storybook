218c71e7054d0691b983d6fbc207bfcf33527852
James Tetley <tetloose@gmail.com>
2025-02-17 20:33:00 +0000

    updated vite config and storybook config to move index exports into own chunks to prevent circular dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,33 @@ export default defineConfig(({ mode }) => {
     build: {
       manifest: true,
       sourcemap: mode === 'development',
-      outDir: resolve(__dirname, 'build')
+      outDir: resolve(__dirname, 'build'),
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            const chunks: { [key: string]: string } = {
+              [resolve(__dirname, 'src/foundations')]: 'foundations',
+              [resolve(__dirname, 'src/atoms')]: 'atoms',
+              [resolve(__dirname, 'src/components')]: 'components',
+              [resolve(__dirname, 'src/layouts')]: 'layouts',
+              [resolve(__dirname, 'src/hooks')]: 'hooks',
+              [resolve(__dirname, 'src/crud')]: 'crud',
+              [resolve(__dirname, 'src/utils')]: 'utils',
+              [resolve(__dirname, 'src/global')]: 'global',
+              [resolve(__dirname, 'src/styles')]: 'styles',
+              [resolve(__dirname, 'src/images')]: 'images',
+              [resolve(__dirname, 'src/constants')]: 'constants',
+              [resolve(__dirname, 'src/routes')]: 'routes'
+            }
+
+            for (const [aliasPath, chunkName] of Object.entries(chunks)) {
+              if (id.includes(aliasPath)) {
+                return chunkName
+              }
+            }
+          }
+        }
+      }
     },
     resolve: {
       alias: {
